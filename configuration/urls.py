35cbbce4da0e262ca0c62bb91e9d2d3e48cba23f
oleg45202 <ogusev@emphasoft.com>
2021-02-19 09:33:46 +0000

    Initial commit

---
+++ configuration/urls.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path, include
+from django.views.static import serve
+
+urlpatterns = [
+    path('', include('core.urls')),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})
+]
